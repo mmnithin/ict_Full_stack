e2277b285aabea07c5630761f14c7e0dde51f74a
ubuntu <mm.nithin@gmail.com>
2018-11-09 14:51:10 +0530

    3rd day

--- html_css/js/script.js
+++ html_css/js/script.js
@@ -1,23 +1,125 @@
-function read(){
+ function read(){
 
     var name= document.getElementById("get_name").value;
     var rollNumber= document.getElementById("get_rollno").value;
     var addNumber =document.getElementById("get_addno").value;
     var addAge =document.getElementById("get_age").value;
+
+    var option =document.getElementById("get_district");
+    var dist=option.options[option.selectedIndex].value;
+   
+
+
+    
+    
+    console.log(name);
+    console.log(rollNumber);
+    console.log(addNumber);
+    console.log(dist);
     if(addAge<18){
         window.alert("You are Not Eligible");
     }else{
         window.alert("You are  Eligible");
         console.log(addAge);
     }
+ }
+
+    function oper(){
+
+        var firstNo=document.getElementById("get_first_no").value;
+        var secondNo=document.getElementById("get_second_no").value;
     
-    console.log(name);
-    console.log(rollNumber);
-    console.log(addNumber);
-    
-    
-    
-    
-    
-    
+        var x= parseInt(firstNo);
+        var y =parseInt(secondNo);
+
+        var op=document.getElementById("operation");
+        var select=op.options[op.selectedIndex].value;
+
+        if(select== "add"){
+
+            var result=x+y;
+        }else if(select =="sub"){
+            var result=x-y;
+        }else if(select=="mul"){
+            var result=x*y;
+        }else{
+            
+
+                var result=x/y;
+            
+            
+        }
+        
+        
+        console.log(result);
+        window.alert(result);
+    }
+
+    function big(){
+
+        var n1= document.getElementById("first_num").value;
+        var n2=document.getElementById("second_num").value;
+        var n3=document.getElementById("third_num").value;
+
+        var num1= parseInt(n1);
+        var num2= parseInt(n2);
+        var num3= parseInt(n3);
+
+         if (num1>num2){
+             if(num1>num3){
+                console.log("Largest number is" + num1);
+               window.alert("Largest number is" +num1);
+             }else{
+                console.log("Largest number is" + num3);
+               window.alert("Largest number is" +num3);
+
+             }
+           
+         }else{
+             if(num2>num3){
+
+                console.log("Largest number is" + num2);
+          window.alert("Largest number is" +num2);
+             }else{
+
+                console.log( "Largest number is" +num3);
+               window.alert("Largest number is" +num3);
+             }
+            
+         }
+        
+    }
+    function small(){
+
+        var n1= document.getElementById("first_num").value;
+        var n2=document.getElementById("second_num").value;
+        var n3=document.getElementById("third_num").value;
+
+        var num1= parseInt(n1);
+        var num2= parseInt(n2);
+        var num3= parseInt(n3);
+
+         if (num1<num2){
+             if(num1<num3){
+                console.log("Largest number is" + num1);
+               window.alert("Largest number is" +num1);
+             }else{
+                console.log("Largest number is" + num3);
+               window.alert("Largest number is" +num3);
+
+             }
+           
+         }else{
+             if(num2<num3){
+
+                console.log("Largest number is" + num2);
+          window.alert("Largest number is" +num2);
+             }else{
+
+                console.log( "Largest number is" +num3);
+               window.alert("Largest number is" +num3);
+             }
+            
+         }
+        
     }
